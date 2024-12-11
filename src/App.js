801dd398f0422291251fce87d92ec2a47861f862
dhanshri <nashtedhanashri0707@gmail.com>
2024-12-12 01:38:43 +0530

    my project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Hero from './Componenets/Hero'
+import Program from './Componenets/Programs/Program.jsx';
+import Reasons from './Componenets/Reasons/Reasons.jsx';
+import Plans from './Componenets/Plans/Plans.jsx';
+import Testimonial from './Componenets/Testimonials/Testimonial.jsx';
+import Joins from './Componenets/Joins/Joins.jsx';
+import Footer from './Componenets/Footer/Footer.jsx';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+          <Hero></Hero>
+          <Program></Program>
+          <Reasons></Reasons>
+          <Plans></Plans>
+          <Testimonial></Testimonial>
+          <Joins></Joins>
+          <Footer></Footer>
     </div>
   );
 }
